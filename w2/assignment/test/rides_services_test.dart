4b95abc58b4f0632d0d7e4781aaa9185035a0e80
Choeng-Rayu <choengrayu307@gmail.com>
2026-02-05 13:06:18 +0700

    bla 001

--- w2/assignment/test/rides_services_test.dart
+++ w2/assignment/test/rides_services_test.dart
@@ -5,30 +5,30 @@ import 'package:blabla/model/ride/locations.dart';
 import 'package:blabla/services/rides_service.dart';
 
 void main() {
-  RidesService.allRides;
-  print(RidesService.allRides);
+  RidesService.totalRides;
+  print(RidesService.totalRides);
 }
 
 class RidesService {
-  static List<Ride> allRides = fakeRides;
+  static List<Ride> totalRides = fakeRides;
 
   // TODO Create a static method to filter rides by departure location
   static List<Ride> filterByDeparture(Location departure) {
-    return allRides
+    return totalRides
         .where((ride) => ride.departureLocation == (departure))
         .toList();
   }
 
   // TODO Create a static method to filter rides by number of requested seat
   static List<Ride> filterBySeatRequested(int seatRequested) {
-    return allRides
+    return totalRides
         .where((ride) => ride.remainingSeats >= seatRequested)
         .toList();
   }
 
   // TODO Create a static method to filter : optional departure location, optional requested seat
   static List<Ride> filterBy({Location? departure, int? seatRequested}) {
-    return allRides.where((ride) {
+    return totalRides.where((ride) {
       if (departure != null && ride.departureLocation != departure) {
         return false;
       }
@@ -41,3 +41,5 @@ class RidesService {
     }).toList();
   }
 }
+
+
