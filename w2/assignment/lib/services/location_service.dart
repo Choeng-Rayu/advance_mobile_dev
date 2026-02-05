7cec6a9d40e4c2ba64827ec522d424e3398f9148
Choeng-Rayu <choengrayu307@gmail.com>
2026-02-05 14:15:33 +0700

    bla 001

--- w2/assignment/lib/services/location_service.dart
+++ w2/assignment/lib/services/location_service.dart
@@ -1,13 +1,45 @@
 import 'package:blabla/data/dummy_data.dart';
 
 import '../model/ride/locations.dart';
+import '../model/ride/ride.dart';
 
 ////
 ///   This service handles:
 ///   - The list of available rides
 ///
-class LocationsService {
-  static const List<Location> availableLocations = fakeLocations; 
-  // TODO for now fake data
-  // List<Ride> filterByDeparture(Location departure)
+
+class RidesService {
+  static List<Ride> totalRides = fakeRides;
+
+  // TODO Create a static method to filter rides by departure location
+  static List<Ride> filterByDeparture(Location departure) {
+    return totalRides
+        .where((ride) => ride.departureLocation == (departure))
+        .toList();
+  }
+
+  // TODO Create a static method to filter rides by number of requested seat
+  static List<Ride> filterBySeatRequested(int seatRequested) {
+    return totalRides
+        .where((ride) => ride.remainingSeats >= seatRequested)
+        .toList();
+  }
+
+  // TODO Create a static method to filter : optional departure location, optional requested seat
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+    return totalRides.where((ride) {
+      if (departure != null && ride.departureLocation != departure) {
+        return false;
+      }
+
+      if (seatRequested != null && ride.remainingSeats < seatRequested) {
+        return false;
+      }
+
+      return true;
+    }).toList();
+  }
 }
+
+
+
