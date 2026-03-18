8b59771c83a20762ba916a7384224ec6ccaf4544
Choeng-Rayu <choengrayu307@gmail.com>
2026-03-18 21:19:02 +0700

    BLA-203: Global RidePreferenceState

--- w8/before_practice/lib_refactor_MVVM/model/ride_preference.dart
+++ w8/before_practice/lib_refactor_MVVM/model/ride_preference.dart
@@ -1,7 +1,17 @@
+import 'location.dart';
+
+///
+/// This model describes a ride preference.
+/// A ride preference consists of the selection of a departure + arrival + a date and a number of passenger
+/// Additional preferences include filters for car types, amenities, and pricing
+///
 class RidePreference {
   final String id;
   final String userId;
-  final int preferredSeats;
+  final Location departure;
+  final DateTime departureDate;
+  final Location arrival;
+  final int requestedSeats;
   final List<String> acceptedCarTypes;
   final double maxPrice;
   final bool musicAllowed;
@@ -12,16 +22,43 @@ class RidePreference {
   RidePreference({
     required this.id,
     required this.userId,
-    required this.preferredSeats,
-    required this.acceptedCarTypes,
-    required this.maxPrice,
-    required this.musicAllowed,
-    required this.petsAllowed,
-    required this.smokingAllowed,
-    required this.chattingPreference,
+    required this.departure,
+    required this.departureDate,
+    required this.arrival,
+    required this.requestedSeats,
+    this.acceptedCarTypes = const [],
+    this.maxPrice = 200,
+    this.musicAllowed = false,
+    this.petsAllowed = false,
+    this.smokingAllowed = false,
+    this.chattingPreference = false,
   });
 
   @override
   String toString() =>
-      'RidePreference(id: $id, userId: $userId, preferredSeats: $preferredSeats)';
+      'RidePreference(departure: ${departure.name}, arrival: ${arrival.name}, '
+      'departureDate: ${departureDate.toIso8601String()}, requestedSeats: $requestedSeats)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is RidePreference &&
+        other.id == id &&
+        other.userId == userId &&
+        other.departure == departure &&
+        other.departureDate == departureDate &&
+        other.arrival == arrival &&
+        other.requestedSeats == requestedSeats;
+  }
+
+  @override
+  int get hashCode => Object.hash(
+    id,
+    userId,
+    departure,
+    departureDate,
+    arrival,
+    requestedSeats,
+  );
 }
