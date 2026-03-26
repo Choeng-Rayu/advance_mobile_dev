c7767164db7eaa8e6c6cde2910830bf3022975ca
Choeng-Rayu <choengrayu307@gmail.com>
2026-03-26 08:28:43 +0700

    w9-01

--- w9/assignment/fire_practice/lib/data/dtos/song_dto.dart
+++ w9/assignment/fire_practice/lib/data/dtos/song_dto.dart
@@ -2,21 +2,23 @@
 
 class SongDto {
   static const String idKey = 'id';
-  static const String titleKey = 'name';
-  static const String artistKey = 'artists';
-  static const String durationKey = 'durationMs';   // in ms
+  static const String titleKey = 'title';
+  static const String artistKey = 'artistId';
+  static const String durationKey = 'duration';   // in ms
+  static const String imageUrlKey = 'imageUrl';
 
-  static Song fromJson(Map<String, dynamic> json) {
-    assert(json[idKey] is String);
+  static Song fromJson(String id, Map<String, dynamic> json) {
+    // assert(json[idKey] is String);
     assert(json[titleKey] is String);
     assert(json[artistKey] is String);
     assert(json[durationKey] is int);
 
     return Song(
-      id: json[idKey],
+      id: id,
       title: json[titleKey],
       artist: json[artistKey],
       duration: Duration(milliseconds: json[durationKey]),
+      imageUrl: Uri.parse(imageUrlKey)
     );
   }
 
@@ -27,6 +29,7 @@ class SongDto {
       titleKey: song.title,
       artistKey: song.artist,
       durationKey: song.duration.inMilliseconds,
+      imageUrlKey: song.imageUrl
     };
   }
 }
--- w9/assignment/fire_practice/lib/data/repositories/songs/song_repository_firebase.dart
+++ w9/assignment/fire_practice/lib/data/repositories/songs/song_repository_firebase.dart
@@ -7,7 +7,10 @@ import '../../dtos/song_dto.dart';
 import 'song_repository.dart';
 
 class SongRepositoryFirebase extends SongRepository {
-  final Uri songsUri = Uri.https('YOUR FIREBASE URL', '/songs.json');
+  final Uri songsUri = Uri.https(
+    'week-8-practice-20451-default-rtdb.asia-southeast1.firebasedatabase.app',
+    '/week9/songs.json',
+  );
 
   @override
   Future<List<Song>> fetchSongs() async {
@@ -15,8 +18,18 @@ class SongRepositoryFirebase extends SongRepository {
 
     if (response.statusCode == 200) {
       // 1 - Send the retrieved list of songs
-      List<dynamic> songJson = json.decode(response.body);
-      return songJson.map((item) => SongDto.fromJson(item)).toList();
+      final decode = json.decode(response.body);
+      final Map<String, dynamic> songJson = Map<String, dynamic>.from(decode);
+      List<Song> songs = [];
+
+      songJson.forEach((key, value) {
+        final songJson = Map<String, dynamic>.from(value as Map);
+        songs.add(SongDto.fromJson(key, songJson));
+      });
+
+      return songs;
+
+      // return songJson.map((item) => SongDto.fromJson(key, item)).toList();
     } else {
       // 2- Throw expcetion if any issue
       throw Exception('Failed to load posts');
